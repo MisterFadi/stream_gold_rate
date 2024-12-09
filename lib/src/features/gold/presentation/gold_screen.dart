be5b8867b7281bfa67852c1fbdf86f33fe109877
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-12-09 11:33:12 +0100

    add StreamBuilder

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
@@ -7,7 +8,7 @@ class GoldScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     /// Platzhalter für den Goldpreis
     /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
+    //const double goldPrice = 69.22;
 
     return SafeArea(
       child: Scaffold(
@@ -21,15 +22,32 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
+              // Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
               // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
-              ),
+
+              StreamBuilder<double>(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (!snapshot.hasData) {
+                    return const Text("Keine Daten verfügbar");
+                  } else if (snapshot.hasError) {
+                    return Text("Error 404: ${snapshot.error}");
+                  } else {
+                    final goldPrice = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(locale: 'de_DE')
+                          .format(goldPrice),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineLarge!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.primary),
+                    );
+                  }
+                },
+              )
             ],
           ),
         ),
